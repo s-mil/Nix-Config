acd1d76bac1b402f5f2c4a8bff47798a8f79cab9
Sam Miller <smiller2033@outlook.com>
2023-07-03 19:27:30 -0500

    fix home-manager

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      "sithise@thor" = home-manager.lib.homeManagerConfiguration {
+      "sithis@thor" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
