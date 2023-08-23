eebb4484533806e80e9940785c02c7678a3ffa3a
Sam Miller <smiller2033@outlook.com>
2023-08-22 20:17:58 -0500

    Push current state

--- flake.nix
+++ flake.nix
@@ -7,11 +7,13 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # Home manager
     home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hardware.url = "github:nixos/nixos-hardware";
 
     nix-colors.url = "github:misterio77/nix-colors";
+
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
 
       # color scheme - catppuccin
     catppuccin-btop = {
@@ -57,7 +59,50 @@
 
   };
 
-  outputs = { nixpkgs, nixpkgs-unstable, home-manager, nixos-hardware, self, ... }@inputs : {
+  outputs = inputs{ self
+    , nixpkgs, nixpkgs-unstable, 
+    home-manager, nixos-hardware, vscode-server, ... }: 
+    
+    let
+      inputs = { inherit home-manager nixpkgs nixkpkgs-unstable; };
+
+      genPkgs = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      nixosSystem = system: hostName: username:
+        let
+          pkgs = genPkgs system;
+        in
+          nixpkgs.lib.nixosSystem
+          {
+            inherit system;
+            modules = [
+              { _module.args = { unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${system}; }; }
+              vscode-server.nixosModules.default
+              home-manager.nixosModules.home-manager
+              {
+                networking.hostName = hostName;
+                home-manager.useGlobalPkgs = true;
+                home-manger.useUserPackages = true;
+                home-manager.users.${username} = { imports = [ ./users/${username}.nix ]; };
+              }
+              ./common.nix
+              ./devices/${hostName}
+              ./devices/hardware-configurations/${hostName}
+            ];
+          };
+
+  in
+  {
+    nixosConfigurations ={
+      thor = "x86_64-linux" "thor" "sithis";
+      odin = "x86_64-linux" "odin" "sithis";
+    };
+  };
+
+
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
 
@@ -70,33 +115,33 @@
     # }
 
 
-    nixosConfigurations.mjolnr = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { 
-          inherit inputs;
-        }; # Pass flake inputs to our config
-        modules = [ 
-          home-manager.nixosModules.home-manager
-          ./common.nix
-          ./devices/prox.nix
-          ./devices/hardware-configurations/prox.nix
-        ];
+    # nixosConfigurations.mjolnr = nixpkgs.lib.nixosSystem {
+    #     system = "x86_64-linux";
+    #     specialArgs = { 
+    #       inherit inputs;
+    #     }; # Pass flake inputs to our config
+    #     modules = [ 
+    #       home-manager.nixosModules.home-manager
+    #       ./common.nix
+    #       ./devices/prox.nix
+    #       ./devices/hardware-configurations/prox.nix
+    #     ];
 
-    };
+    # };
 
-    nixosConfigurations.thor = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { 
-          inherit inputs;
-        }; # Pass flake inputs to our config
-        modules = [ 
-          nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga
-          home-manager.nixosModules.home-manager
-          ./common.nix
-          ./devices/lenovoLaptop.nix
-          ./devices/hardware-configurations/lenovoLaptop.nix
-        ];
+    # nixosConfigurations.thor = nixpkgs.lib.nixosSystem {
+    #     system = "x86_64-linux";
+    #     specialArgs = { 
+    #       inherit inputs;
+    #     }; # Pass flake inputs to our config
+    #     modules = [ 
+    #       nixos-hardware.nixosModules.lenovo-thinkpad-x1-yoga
+    #       home-manager.nixosModules.home-manager
+    #       ./common.nix
+    #       ./devices/lenovoLaptop.nix
+    #       ./devices/hardware-configurations/lenovoLaptop.nix
+    #     ];
 
-    };
-  };
+    # };
+#   };
 }
---
+++ nix-flake-ironic.nix
@@ -0,0 +1,93 @@
+{
+  inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+      nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+      nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+
+      vscode-server.url = "github:nix-community/nixos-vscode-server";
+      
+      home-manager.url = "github:nix-community/home-manager/release-23.05";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      
+      nix-darwin.url = "github:lnl7/nix-darwin";
+      nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self
+    , nixpkgs, nixpkgs-unstable, nixpkgs-darwin
+    , home-manager, nix-darwin, vscode-server, ... }:
+    let  
+      inputs = { inherit nix-darwin home-manager nixpkgs nixpkgs-unstable; };
+      # creates correct package sets for specified arch
+      genPkgs = system: import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      genDarwinPkgs = system: import nixpkgs-darwin {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      
+    
+      # creates a nixos system config
+      nixosSystem = system: hostName: username:
+        let
+          pkgs = genPkgs system;
+        in
+          nixpkgs.lib.nixosSystem
+          {
+            inherit system;
+            modules = [
+              # adds unstable to be available in top-level evals (like in common-packages)
+              { _module.args = { unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${system}; }; }
+
+              ./hosts/nixos/${hostName} # ip address, host specific stuff
+              vscode-server.nixosModules.default
+              home-manager.nixosModules.home-manager
+              {
+                networking.hostName = hostName;
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.${username} = { imports = [ ./home/${username}.nix ]; };
+              }
+              ./hosts/common/nixos-common.nix
+            ];
+          };
+
+      # creates a macos system config
+      darwinSystem = system: hostName: username:
+        let
+          pkgs = genDarwinPkgs system;
+        in
+          nix-darwin.lib.darwinSystem 
+          {
+            inherit system inputs;
+            modules = [
+              # adds unstable to be available in top-level evals (like in common-packages)
+              { _module.args = { unstablePkgs = inputs.nixpkgs-unstable.legacyPackages.${system}; }; }
+
+              ./hosts/darwin/${hostName} # ip address, host specific stuff
+              home-manager.darwinModules.home-manager 
+              {
+                networking.hostName = hostName;
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.${username} = { imports = [ ./home/${username}.nix ]; };
+              }
+              ./hosts/common/darwin-common.nix
+            ];
+          };
+    in
+    {
+      darwinConfigurations = {
+        magrathea = darwinSystem "aarch64-darwin" "magrathea" "alex";
+        slartibartfast = darwinSystem "aarch64-darwin" "slartibartfast" "alex";
+        cat-laptop = darwinSystem "aarch64-darwin" "cat-laptop" "alex";
+      };
+
+      nixosConfigurations = {
+        testnix = nixosSystem "x86_64-linux" "testnix" "alex";
+      };
+    };
+
+}
