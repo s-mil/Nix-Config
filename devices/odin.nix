dabd3f80b117ef388f3cdf48d0213b99208215b7
Sam Miller <sam@sammiller.dev>
2023-08-25 12:15:14 -0500

    Add nvida gpu support

--- devices/odin.nix
+++ devices/odin.nix
@@ -25,4 +25,35 @@ in
   virtualisation.docker.enable= true;
 
   programs.steam.enable = true;
+
+
+  # Enable OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is needed most of the time
+    modesetting.enable = true;
+
+	# Enable power management (do not disable this unless you have a reason to).
+	# Likely to cause problems on laptops and with screen tearing if disabled.
+	powerManagement.enable = true;
+
+    # Use the open source version of the kernel module ("nouveau")
+	# Note that this offers much lower performance and does not
+	# support all the latest Nvidia GPU features.
+	# You most likely don't want this.
+    # Only available on driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
 }
