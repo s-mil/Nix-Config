f166632101b6a719572529d96684320f7e45a37e
Sam Miller <smiller2033@outlook.com>
2023-09-30 20:26:00 -0500

    update vscode

--- flake.nix
+++ flake.nix
@@ -3,11 +3,11 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/23.11-pre";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     # Home manager
     home-manager.url = "github:nix-community/home-manager/master";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hardware.url = "github:nixos/nixos-hardware";
 
