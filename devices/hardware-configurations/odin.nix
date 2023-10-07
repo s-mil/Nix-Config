0270f7dc66d16b0462b76ed6e1634eb9b9b614ce
Sam Miller <sam@sammiller.dev>
2023-10-07 11:02:09 -0500

    add odin nfs mount

--- devices/hardware-configurations/odin.nix
+++ devices/hardware-configurations/odin.nix
@@ -27,6 +27,12 @@
     [ { device = "/dev/disk/by-uuid/aa6e7e5a-b7ef-4231-a158-88590ffdd5cb"; }
     ];
 
+
+  fileSystems."/mnt/Digital-Paper" =
+    { device = "10.10.0.10:/mnt/Data/Media/Digital-Paper";
+      fsType = "nfs";
+    };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
