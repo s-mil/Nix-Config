02aa3e6bb4297a87b5ffa7fec2484db2a5c95bb4
Sam Miller <smiller2033@outlook.com>
2023-11-09 21:05:49 -0600

    fix code

--- users/sithis/vscode.nix
+++ users/sithis/vscode.nix
@@ -3,6 +3,6 @@
 {
     programs.vscode = {
         enable = true;
-        package = unstable.vscode.fhsWithPackages (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
+        package = unstable.vscode;
     };
 }
