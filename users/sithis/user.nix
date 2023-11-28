9ccd43cd9648fc40ce1d1b7a34734668c92b845d
Sam Miller <smiller2033@outlook.com>
2023-11-28 05:19:15 -0600

    add auth key

--- users/sithis/user.nix
+++ users/sithis/user.nix
@@ -4,7 +4,7 @@
         isNormalUser =  true;
         shell = pkgs.zsh;
         extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ];
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ1lOJawuuRoM4C3OWa/Bu+Mz9tzafFtoCg04c6B4fi sithpow@gmail.com" ];
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQ1lOJawuuRoM4C3OWa/Bu+Mz9tzafFtoCg04c6B4fi sithpow@gmail.com" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILVrEbYe/PAtsQ4q5+zr/Dy1Zu3NK92apcBcn2Igze4N sam@neumann-miller.com" ];
         
     };
 
