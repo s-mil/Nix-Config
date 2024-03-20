fde025efc01d6e67140213fcfa32ed0250cc31ba
Sam Miller <smiller2033@outlook.com>
2024-03-19 21:08:26 -0500

    add intel driver for freya

--- devices/freya.nix
+++ devices/freya.nix
@@ -1,47 +1,56 @@
-{ config, pkgs, pkgs-unstable,lib, inputs, ... }:
+{ config, pkgs, pkgs-unstable, lib, inputs, ... }:
 let
 
   Hostname = "freya";
 
-in
-{
-    networking =
-    {
-      hostName = Hostname;
-      hostId = "007f0202";
-      firewall.enable = false;
-      interfaces= {
-        eno1 = {
-          useDHCP = false;
-          ipv4.addresses = [ {
-            address = "10.10.0.11";
-            prefixLength = 16;
-          } ];
-        };
+in {
+  networking = {
+    hostName = Hostname;
+    hostId = "007f0202";
+    firewall.enable = false;
+    interfaces = {
+      eno1 = {
+        useDHCP = false;
+        ipv4.addresses = [{
+          address = "10.10.0.11";
+          prefixLength = 16;
+        }];
       };
-      defaultGateway = "10.0.0.1";
-      nameservers = [ "1.1.1.1" ];
     };
-    imports =
-    [
-        ../common.nix
-        ../bootloaders/systemd.nix
-        ../displayManager/sddm.nix
-        ../windowManager/sway.nix
-        ../desktopEnvironment/kde.nix
-        ../users/root/user.nix
-        ../users/sithis/user.nix
-        ../tweaks/brother_printer.nix
-    ];
-  
+    defaultGateway = "10.0.0.1";
+    nameservers = [ "1.1.1.1" ];
+  };
+  imports = [
+    ../common.nix
+    ../bootloaders/systemd.nix
+    ../displayManager/sddm.nix
+    ../windowManager/sway.nix
+    ../desktopEnvironment/kde.nix
+    ../users/root/user.nix
+    ../users/sithis/user.nix
+    ../tweaks/brother_printer.nix
+  ];
 
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.sddm.enableGnomeKeyring = true;
 
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  }; # Force intel-media-driver
+
   virtualisation.docker = {
-    enable= true;
+    enable = true;
     autoPrune = {
       enable = true;
       dates = "weekly";
@@ -50,9 +59,7 @@ in
 
   services.openssh = {
     enable = true;
-    settings = {
-      PasswordAuthentication = false;
-    };
+    settings = { PasswordAuthentication = false; };
   };
 
   services.k3s = {
