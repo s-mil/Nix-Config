1cca451675417dbe1a2deb2e680d0ea0859da921
Sam Miller <smiller2033@outlook.com>
2024-03-29 16:52:10 -0500

    add some udev for via for a time

--- devices/odin.nix
+++ devices/odin.nix
@@ -90,4 +90,7 @@ in {
     motherboard = "amd";
 
   };
+  services.udev.extraRules = ''
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{serial}=="*vial:f64c2b3c*", MODE="0660", GROUP="users", TAG+="uaccess", TAG+="udev-acl"
+''; 
 }
