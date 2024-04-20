0d6235248b32b40a2413990114ea0cece22ac0fc
Sam Miller <smiller2033@outlook.com>
2024-04-20 09:17:39 -0500

    fix

--- hosts/common/optional/laptops/default.nix
+++ hosts/common/optional/laptops/default.nix
@@ -1,7 +1,7 @@
 { inputs, outputs, ... }: {
   imports = [
     ./laptop.nix
-    ./keymap.nix
+    # ./keymap.nix not sure why this nolonger works
 
   ] ++ (builtins.attrValues outputs.nixosModules);
 }
