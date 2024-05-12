41bfa0e1eacc1eead4677767d4520e9b1b596651
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-05-12 02:09:47 -0500

    beta nvida

--- hosts/common/optional/nvidia.nix
+++ hosts/common/optional/nvidia.nix
@@ -7,7 +7,7 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
