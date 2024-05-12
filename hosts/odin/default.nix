9ac549c070130cd7813d04bace17e2981ceb6f35
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-05-12 02:17:18 -0500

    try this

--- hosts/odin/default.nix
+++ hosts/odin/default.nix
@@ -84,7 +84,7 @@ in {
   services.xserver.displayManager.sddm = {
     enable = true;
     theme = "catppuccin-sddm-corners";
-    settings = { input = ""; };
+    settings = { input = null; };
 
   };
 
