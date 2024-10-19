b83e0255cac846877c3918cad78552687cc571e4
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-10-19 16:47:11 -0500

    update timer for forget as well

--- hosts/common/optional/services/rustic-container-timer.nix
+++ hosts/common/optional/services/rustic-container-timer.nix
@@ -1,8 +1,18 @@
-{pkgs, ...}:{
+{pkgs, ...}:
+let 
+schedule = "daily";
+AccuracySec = "1m";
+RandomizedDelaySec = "1h";
+
+in 
+
+{
   systemd.timers."Backup-Containers" = {
     wantedBy = [ "timers.target"];
       timerConfig = {
-        OnCalendar = "*-*-* 4:00:00";
+        OnCalendar = schedule;
+        AccuracySec = AccuracySec;
+        RandomizedDelaySec = RandomizedDelaySec;
         Persistent = true;
         Unit = "Backup-Containers.service";
       };
@@ -18,4 +28,24 @@
     };
   };
 
+
+  systemd.timers."Backup-Containers-Forget" = {
+       wantedBy = [ "timers.target"];
+        timerConfig = {
+          OnCalendar = schedule;
+          AccuracySec = AccuracySec;
+          RandomizedDelaySec = RandomizedDelaySec;
+          Persistent = true;
+          Unit = "Backup-Containers-Forget.service";
+      };
+  };
+  systemd.services."Backup-Containers-Forget" = {
+      script = ''
+      ${pkgs.rustic}/bin/rustic forget
+      '';
+      serviceConfig = { 
+        Type = "oneshot";
+        User = "sithis"; 
+        };
+  };
 }
