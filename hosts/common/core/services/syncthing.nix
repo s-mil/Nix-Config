d42d094f30d35e81e913dcf56c0c9e85610cb164
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-11-01 07:55:10 -0500

    update default syncthing

--- hosts/common/core/services/syncthing.nix
+++ hosts/common/core/services/syncthing.nix
@@ -2,6 +2,7 @@
   services.syncthing = {
     enable = true;
     dataDir = "/home/sithis/syncthing";
+    databaseDir = "/home/sithis/.config/syncthing";
     user = "sithis";
     group = "users";
 
