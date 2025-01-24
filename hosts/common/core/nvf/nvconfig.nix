47e44c2cf8b41fd4f7eee3df2110edbad08c56a8
Sam Miller <23262411+s-mil@users.noreply.github.com>
2025-01-23 20:23:06 -0600

    Update

--- hosts/common/core/nvf/nvconfig.nix
+++ hosts/common/core/nvf/nvconfig.nix
@@ -1,22 +1,69 @@
 { pkgs, lib, ... }:
 {
   programs.nvf = {
-    package = pkgs.neovim-unwrapped;
     defaultEditor = true;
     enableManpages = true;
     enable = true;
     settings.vim = {
+      statusline = {
+        lualine = {
+          enable = true;
+          theme = "catppuccin";
+        };
+      };
+      nvim-autopairs.enable = true;
+      visuals = {
+        nvim-web-devicons.enable = true;
+        nvim-scrollbar.enable = true;
+        cellular-automaton.enable = false;
+        highlight-undo.enable = true;
+
+        indent-blankline = {
+          enable = true;
+        };
+
+        nvim-cursorline = {
+          enable = true;
+          setupOpts = {
+            line_timeout = 0;
+          };
+        };
+
+        fidget-nvim = {
+          enable = true;
+          setupOpts = {
+            notification.window = {
+              winblend = 0;
+              border = "none";
+            };
+          };
+        };
+      };
+
+      autocomplete.nvim-cmp = {
+        enable = true;
+        mappings = {
+          close = "<C-e>";
+          confirm = "<C-y>";
+          next = "<C-n>";
+          previous = "<C-p>";
+          scrollDocsDown = "<C-j>";
+          scrollDocsUp = "<C-k>";
+        };
+      };
+
+      package = pkgs.neovim-unwrapped;
       viAlias = true;
       vimAlias = true;
       useSystemClipboard = true;
       spellcheck = {
         enable = true;
-        languages = ["en"];
+        languages = [ "en" ];
       };
       theme = {
         enable = true;
         name = "catppuccin";
-        style = "dark";
+        style = "mocha";
       };
 
       languages = {
@@ -24,7 +71,7 @@
         enableDAP = true;
         enableFormat = true;
         enableLSP = true;
-        
+
         nix.enable = true;
         csharp = {
           enable = true;
@@ -44,8 +91,6 @@
         ts.enable = true;
       };
 
-      statusLine.lualine.enable = true;
-      autocomplete.nvim-cmp.enable = true;
     };
   };
 }
