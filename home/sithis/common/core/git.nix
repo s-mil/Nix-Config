e7635ed79ef0d39a683659bb66a8fd3cf7a9b7f7
Sam Miller <23262411+s-mil@users.noreply.github.com>
2025-03-11 14:19:08 -0500

    Update git config

--- home/sithis/common/core/git.nix
+++ home/sithis/common/core/git.nix
@@ -4,12 +4,50 @@
     package = unstable.gitFull;
     userName = "Sam Miller";
     userEmail = "23262411+s-mil@users.noreply.github.com";
+    maintenance.enable = true;
     lfs.enable = true;
+    aliases = {
+      fpush = "push --force-with-lease";
+    };
     extraConfig = {
       credential.helper = "libsecret";
       pull.rebase = true;
       color.ui = true;
       init.defaultBranch = "main";
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      push = {
+        default = "simple";
+        followTags = true;
+        autoSetupRemote = true;
+      };
+      fetch = {
+        prune = true;
+        pruneTags = true;
+        all = true;
+      };
+      help.autocorrect = "prompt";
+      commit.verbose = true;
+      rebase = {
+        autoStash = true;
+        autoSquash = true;
+        updateRefs = true;
+      };
+      merge.conflictstyle = "zdiff3";
+      maintenance = {
+        gc = "disabled";
+        commit-graph = "hourly";
+        prefetch = "hourly";
+        loose-objects = "daily";
+        incremental-repack = "daily";
+      };
     };
   };
 
