2c936d34d2ba31e5c2ce18c121724cf195e089f8
Sam Miller <23262411+s-mil@users.noreply.github.com>
2025-04-20 14:51:03 -0500

    remove nvf

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -1,37 +1,41 @@
-{ inputs, outputs, ... }: {
-  imports = [
-    inputs.home-manager.nixosModules.home-manager
-    ./locale.nix # localization settings
-    ./nix.nix # nix settings and garbage collection
-    ./zsh.nix # load a basic shell just in case we need it without home-manager
-    ./standardPkgs.nix # base packages on a system
-    ./services/tailscale.nix # tailscale for interdevice networking
-    ./services/rustic.nix # Rustic for backups
-    ./services/syncthing.nix # syncthing for file syncing
-    ./fonts.nix # fonts
-    ./nh.nix
-    ./nvf # Neovim config
-    #./services/auto-upgrade.nix # auto-upgrade service
-  ] ++ (builtins.attrValues outputs.nixosModules);
+{
+  inputs,
+  outputs,
+  ...
+}: {
+  imports =
+    [
+      inputs.home-manager.nixosModules.home-manager
+      ./locale.nix # localization settings
+      ./nix.nix # nix settings and garbage collection
+      ./zsh.nix # load a basic shell just in case we need it without home-manager
+      ./standardPkgs.nix # base packages on a system
+      ./services/tailscale.nix # tailscale for interdevice networking
+      ./services/rustic.nix # Rustic for backups
+      ./services/syncthing.nix # syncthing for file syncing
+      ./fonts.nix # fonts
+      ./nh.nix
+      #./services/auto-upgrade.nix # auto-upgrade service
+    ]
+    ++ (builtins.attrValues outputs.nixosModules);
 
-  home-manager.extraSpecialArgs = { inherit inputs outputs; };
+  home-manager.extraSpecialArgs = {inherit inputs outputs;};
 
   nixpkgs = {
     # you can add global overlays here
     overlays = builtins.attrValues outputs.overlays;
-    config = { allowUnfree = true; };
+    config = {allowUnfree = true;};
   };
 
   networking.timeServers = [
-  "time.cloudflare.com"
+    "time.cloudflare.com"
   ];
 
-  services.udisks2 = { enable = true; };
+  services.udisks2 = {enable = true;};
   hardware.enableRedistributableFirmware = true;
   security.sudo.enable = true;
   # Allow members of the "wheel" group to sudo:
   security.sudo.configFile = ''
     %wheel ALL=(ALL) ALL
   '';
-
 }
