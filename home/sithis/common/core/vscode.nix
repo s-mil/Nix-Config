643588eefb78ae0ee9692ea97cffa6529b70a4f9
Sam Miller <23262411+s-mil@users.noreply.github.com>
2025-07-23 09:46:59 -0500

    Update

--- home/sithis/common/core/vscode.nix
+++ home/sithis/common/core/vscode.nix
@@ -3,7 +3,7 @@
 {
   programs.vscode = {
     enable = true;
-    package = unstable.vscode-fhs;
+    package = unstable.vscode.fhsWithPackages (ps: with ps; [ rustup zlib openssl.dev pkg-config ]);
   };
 
 }
