2fd4a75ed132588772650df0df49bf81ee2bd6c6
Sam Miller <23262411+s-mil@users.noreply.github.com>
2026-02-21 10:10:28 -0600

    Update ollama models

--- hosts/common/optional/services/ollama.nix
+++ hosts/common/optional/services/ollama.nix
@@ -2,17 +2,18 @@
 
   services.ollama = {
     enable = true;
-    acceleration = "cuda";
+    package = pkgs.ollama-cuda;
     home = "/llm";
     models = "/llm/models";
     port = 11434;
     host = "0.0.0.0";
     loadModels = [
         "llama3.2:latest"
-        "codellama:latest"
-        "mistral:latest"
+        "mistral-3:8b"
         "deepseek-r1:8b"
+        "glm-4.7-flash:q8_0"
         "gpt-oss:20b"
+        "qwen3:8b"
 ];
   };
 
